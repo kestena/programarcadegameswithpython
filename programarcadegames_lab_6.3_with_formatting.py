dda3e64ff0e6c3aa0112e0e3b5b502df05b3fe32
kestena <37227049+kestena@users.noreply.github.com>
2018-09-07 23:10:35 +0200

    New version
    In the previous version I used custom ranges to count back/upwards. But after some hint from a reddit user, I was able to change those to normal non-custom ranges.

--- programarcadegames_lab_6.3_with_formatting.py
+++ programarcadegames_lab_6.3_with_formatting.py
@@ -1,30 +1,25 @@
 user_choice = int(input("Please enter a number: "))
 m = 2*user_choice
-cust_range1 = 1
-cust_range2 = 2
 #triangle 1
 for i in range (0, user_choice):
-    for j in range (cust_range1, m, 2):
+    for j in range ((i*2)+1, m, 2):
         print ("{:2}".format(j), end=" ")
     for j in range (i):
         print ("{:2}".format(" "), end=" ")
     for j in range (i):
         print ("{:2}".format(" "), end= " ")
-    for j in range (m-1, cust_range1-1, -2):
+    for j in range (m-1, i*2, -2):
         print ("{:2}".format(j), end=" ")
     print()
-    cust_range1 += 2
 
 #triangle 2  
 for i in range (0, user_choice):
-    for j in range (m-1, m-cust_range2, -2):
+    for j in range ((m-1)-i*2, m, 2):
         print ("{:2}".format(j), end=" ")
     for j in range (1, user_choice-i):
         print ("{:2}".format(" "), end=" ")
     for j in range (1, user_choice-i):
         print ("{:2}".format(" "), end=" ")
-    for j in range (m-1, m-cust_range2, -2):
+    for j in range (m-1, (m-2)-i*2, -2):
         print ("{:2}".format(j), end=" ")
     print()
-    cust_range2 += 2
-    
